7ef4d18a62ea809c7d7bb1e8f96d8f1e25276e31
ontiverosj <68527652+ontiverosj@users.noreply.github.com>
2026-09-15 17:34:14 -0400

    Add Agent Edit support to electron/preload.cjs

--- electron/preload.cjs
+++ electron/preload.cjs
@@ -2,6 +2,8 @@ const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('studio', {
   importMedia: () => ipcRenderer.invoke('media:import'),
+  probeMedia: (filePath) => ipcRenderer.invoke('media:probe', filePath),
+  autoEdit: (items) => ipcRenderer.invoke('agent:auto-edit', items),
   exportVideo: (payload) => ipcRenderer.invoke('video:export', payload),
   saveProject: (project) => ipcRenderer.invoke('project:save', project),
   openProject: () => ipcRenderer.invoke('project:open'),
